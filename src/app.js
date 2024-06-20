46325801f3652c754afb30fc3a11547abfe95366
sam33r0 <srivastavasameer2002@gmail.com>
2024-06-20 21:31:27 +0530

    testing saveUnitialized

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
 }))
 
 app.use(passport.initialize());
